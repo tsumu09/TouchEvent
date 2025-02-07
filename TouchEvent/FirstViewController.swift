d7887cdaee5ae0a1296b4861154797777dc95417
tsumu09 <tsumu0219@icloud.com>
2025-02-07 19:55:23 +0900

    テキストボックスの追加とアルバム開けないのを修正しました。

--- TouchEvent/FirstViewController.swift
+++ TouchEvent/FirstViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
 
     @IBOutlet var cameraImageView: UIImageView!
+    @IBOutlet weak var textButton: UIButton!
     
     var originalImage: UIImage!
-    
     var filter: CIFilter!
+    var isTextModeActive: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOutSideTextView))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+        
+        textButton.setTitle("Text", for: .normal)
+        textButton.setTitle("Text", for: .selected)
+        textButton.addTarget(self, action: #selector(toggleTextMode), for: .touchUpInside)
+    }
     
     @IBAction func takePhoto() {
         
@@ -28,13 +41,13 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         } else {
             print("error")
         }
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            cameraImageView.image = info [.editedImage] as? UIImage
-            
-            originalImage = cameraImageView.image
-            
-            dismiss(animated: true, completion: nil)
         }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        cameraImageView.image = info [.editedImage] as? UIImage
+        
+        originalImage = cameraImageView.image
+        
+        dismiss(animated: true, completion: nil)
     }
     
 
@@ -87,17 +100,66 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         present(activityViewController, animated: true, completion: nil)
     }
 
-    
-    @IBOutlet weak var textField: UITextField! // UITextFieldのアウトレット
-        @IBOutlet weak var resultLabel: UILabel!   // 結果を表示するUILabelのアウトレット
-
-        @IBAction func buttonPressed() {
-            // ボタンが押されたときの処理
-            if let text = textField.text, !text.isEmpty {
-                resultLabel.text = "入力されたテキスト: \(text)"
-            } else {
-                resultLabel.text = "テキストを入力してください。"
-            }
-        }
+    @objc func toggleTextMode() {
+        isTextModeActive.toggle()
+        textButton.isSelected = isTextModeActive
+    }
         
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: view)
+        
+        if isTextModeActive {
+            addTextView(at: location)
+        }
+    }
+    
+    func addTextView(at location: CGPoint) {
+        let textView = UITextView(frame: CGRect(x: location.x, y: location.y, width: 150, height: 50))
+        textView.textColor = .black
+        textView.font = UIFont.systemFont(ofSize: 18)
+        textView.isEditable = true
+        textView.delegate = self
+        textView.isScrollEnabled = false
+        textView.text = "タップして編集"
+        
+        setActiveTextViewStyle(textView)
+        view.addSubview(textView)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleDrag(_:)))
+        textView.addGestureRecognizer(panGesture)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        setActiveTextViewStyle(textView)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        setInactiveTextViewStyle(textView)
+    }
+    
+    func setActiveTextViewStyle(_ textView: UITextView) {
+        textView.layer.cornerRadius = 5
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.backgroundColor = UIColor.white.withAlphaComponent(0.7)
+    }
+    
+    func setInactiveTextViewStyle(_ textView: UITextView) {
+        textView.layer.borderWidth = 0
+        textView.backgroundColor = UIColor.clear
+    }
+    
+    @objc func handleTapOutSideTextView(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func handleDrag(_ sender: UIPanGestureRecognizer) {
+        guard let textView = sender.view as? UITextView else { return }
+        
+        let translation = sender.translation(in: view)
+        textView.center = CGPoint(x: textView.center.x + translation.x, y: textView.center.y + translation.y)
+        sender.setTranslation(.zero, in: view)
+    }
+    
 }
